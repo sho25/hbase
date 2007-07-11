014ebb84d093862a08fe46f437071a63ba12dc69
Michael Stack <stack@apache.org>
2007-07-11 21:54:15 +0000

    HADOOP-1375 a simple parser for hbase M src/contrib/hbase/NOTICE.txt     Add notice of udanax contributions. Msrc/contrib/hbase/conf/hbase-default.xml     (hbaseshell.jline.bell.enabled): Added. M src/contrib/hbase/CHANGES.txt     (hadoop-1375) Added. M src/contrib/hbase/src/java/org/apache/hadoop/hbase/package.html     Add note on how to start up hbase shell M src/contrib/hbase/bin/hbase     Add 'shell'.  Remove 'client' (shell does what it used do and more).     Removed all reader and logreader until better developed.  Starting     up a reader or logreader on a running hbase system could do damage). M src/contrib/hbase/build.xml     Add a javacc target to generate content of shell/generated subpackage. A src/contrib/hbase/src/test/org/apache/hadoop/hbase/shell/TestHBaseShell.java A src/contrib/hbase/src/java/org/apache/hadoop/hbase/Shell.java A src/contrib/hbase/src/java/org/apache/hadoop/hbase/shell/DeleteCommand.java A src/contrib/hbase/src/java/org/apache/hadoop/hbase/shell/CreateCommand.java A src/contrib/hbase/src/java/org/apache/hadoop/hbase/shell/DropCommand.java A src/contrib/hbase/src/java/org/apache/hadoop/hbase/shell/InsertCommand.java A src/contrib/hbase/src/java/org/apache/hadoop/hbase/shell/CommandFactory.java A src/contrib/hbase/src/java/org/apache/hadoop/hbase/shell/HelpContents.java A src/contrib/hbase/src/java/org/apache/hadoop/hbase/shell/ExitCommand.java A src/contrib/hbase/src/java/org/apache/hadoop/hbase/shell/ConsoleTable.java A src/contrib/hbase/src/java/org/apache/hadoop/hbase/shell/DescCommand.java A src/contrib/hbase/src/java/org/apache/hadoop/hbase/shell/SelectCommand.java A src/contrib/hbase/src/java/org/apache/hadoop/hbase/shell/Command.java A src/contrib/hbase/src/java/org/apache/hadoop/hbase/shell/ShowCommand.java A src/contrib/hbase/src/java/org/apache/hadoop/hbase/shell/BasicCommand.java A src/contrib/hbase/src/java/org/apache/hadoop/hbase/shell/HelpManager.java A src/contrib/hbase/src/java/org/apache/hadoop/hbase/shell/ReturnMsg.java A src/contrib/hbase/src/java/org/apache/hadoop/hbase/shell/HelpCommand.java A src/contrib/hbase/src/java/org/apache/hadoop/hbase/shell/HBaseShell.jj     Added. A src/contrib/hbase/src/java/org/apache/hadoop/hbase/shell/generated/Token.java A src/contrib/hbase/src/java/org/apache/hadoop/hbase/shell/generated/TokenMgrError.java A src/contrib/hbase/src/java/org/apache/hadoop/hbase/shell/generated/SimpleCharStream.java A src/contrib/hbase/src/java/org/apache/hadoop/hbase/shell/generated/ParserTokenManager.java A src/contrib/hbase/src/java/org/apache/hadoop/hbase/shell/generated/ParseException.java A src/contrib/hbase/src/java/org/apache/hadoop/hbase/shell/generated/ParserConstants.java A src/contrib/hbase/src/java/org/apache/hadoop/hbase/shell/generated/Parser.java     Added javacc generated files.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/hadoop/trunk/src/contrib/hbase@555415 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 87f5d5dffd96e09d789a9063f8f6d98a75fb24dd

---
+++ src/java/org/apache/hadoop/hbase/shell/HelpContents.java
@@ -0,0 +1,239 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/**  * Copyright 2007 The Apache Software Foundation  *  * Licensed under the Apache License, Version 2.0 (the "License");  * you may not use this file except in compliance with the License.  * You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
+
+begin_package
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|hadoop
+operator|.
+name|hbase
+operator|.
+name|shell
+package|;
+end_package
+
+begin_import
+import|import
+name|java
+operator|.
+name|util
+operator|.
+name|HashMap
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|util
+operator|.
+name|Map
+import|;
+end_import
+
+begin_class
+specifier|public
+class|class
+name|HelpContents
+block|{
+comment|/**    * add help contents     */
+specifier|public
+specifier|static
+name|Map
+operator|<
+condition|?
+then|extends
+name|String
+operator|,
+operator|?
+expr|extends
+name|String
+index|[]
+operator|>
+name|Load
+argument_list|()
+block|{
+name|Map
+argument_list|<
+name|String
+argument_list|,
+name|String
+index|[]
+argument_list|>
+name|load
+operator|=
+operator|new
+name|HashMap
+argument_list|<
+name|String
+argument_list|,
+name|String
+index|[]
+argument_list|>
+argument_list|()
+block|;
+name|load
+operator|.
+name|put
+argument_list|(
+literal|"SHOW"
+argument_list|,
+operator|new
+name|String
+index|[]
+block|{
+literal|"List all tables."
+block|,
+literal|"SHOW TABLES;"
+block|}
+argument_list|)
+block|;
+name|load
+operator|.
+name|put
+argument_list|(
+literal|"DESCRIBE"
+argument_list|,
+operator|new
+name|String
+index|[]
+block|{
+literal|"Describe a table's columnfamilies."
+block|,
+literal|"DESCRIBE<table_name>;"
+block|}
+argument_list|)
+block|;
+name|load
+operator|.
+name|put
+argument_list|(
+literal|"CREATE"
+argument_list|,
+operator|new
+name|String
+index|[]
+block|{
+literal|"Create a table"
+block|,
+literal|"CREATE<table_name>"
+operator|+
+literal|"\n\t  COLUMNFAMILIES('cf_name1'[, 'cf_name2', ...]);"
+operator|+
+literal|"\n    [LIMIT=versions_limit];"
+block|}
+argument_list|)
+block|;
+name|load
+operator|.
+name|put
+argument_list|(
+literal|"DROP"
+argument_list|,
+operator|new
+name|String
+index|[]
+block|{
+literal|"Drop columnfamilie(s) from a table or drop table(s)"
+block|,
+literal|"DROP table_name1[, table_name2, ...] | cf_name1[, cf_name2, ...];"
+block|}
+argument_list|)
+block|;
+name|load
+operator|.
+name|put
+argument_list|(
+literal|"INSERT"
+argument_list|,
+operator|new
+name|String
+index|[]
+block|{
+literal|"Insert row into table"
+block|,
+literal|"INSERT<table_name>"
+operator|+
+literal|"\n\t('column_name1'[, 'column_name2', ...])"
+operator|+
+literal|"\n\t    VALUES('entry1'[, 'entry2', ...])"
+operator|+
+literal|"\n    WHERE row='row_key';"
+block|}
+argument_list|)
+block|;
+name|load
+operator|.
+name|put
+argument_list|(
+literal|"DELETE"
+argument_list|,
+operator|new
+name|String
+index|[]
+block|{
+literal|"Delete cell or row in table."
+block|,
+literal|"DELETE<table_name>"
+operator|+
+literal|"\n\t    WHERE row='row_key;"
+operator|+
+literal|"\n    [AND column='column_name'];"
+block|}
+argument_list|)
+block|;
+name|load
+operator|.
+name|put
+argument_list|(
+literal|"SELECT"
+argument_list|,
+operator|new
+name|String
+index|[]
+block|{
+literal|"Select values from a table"
+block|,
+literal|"SELECT<table_name>"
+operator|+
+literal|"\n\t    [WHERE row='row_key']"
+operator|+
+literal|"\n    [AND column='column_name'];"
+operator|+
+literal|"\n    [AND time='timestamp'];"
+operator|+
+literal|"\n    [LIMIT=versions_limit];"
+block|}
+argument_list|)
+block|;
+name|load
+operator|.
+name|put
+argument_list|(
+literal|"EXIT"
+argument_list|,
+operator|new
+name|String
+index|[]
+block|{
+literal|"Exit shell"
+block|,
+literal|"EXIT;"
+block|}
+argument_list|)
+block|;
+return|return
+name|load
+return|;
+block|}
+block|}
+end_class
+
+end_unit
+
