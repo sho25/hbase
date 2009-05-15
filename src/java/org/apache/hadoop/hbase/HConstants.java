de0c08b169891aad7cd811dab74ff2a651a24590
Jean-Daniel Cryans <jdcryans@apache.org>
2009-05-15 13:45:11 +0000

    HBASE-1008  [performance] The replay of logs on server crash takes way too long
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hbase/trunk@775132 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: ac347387abc6df4b5179b4255be55d3bfaf66c73

--- src/java/org/apache/hadoop/hbase/HConstants.java
+++ src/java/org/apache/hadoop/hbase/HConstants.java
@@ -323,6 +323,14 @@ literal|1024
 operator|*
 literal|5
 decl_stmt|;
+comment|/** Default number of threads to use when log splitting     *  to rewrite the logs. More means faster but bigger mem consumption */
+specifier|static
+specifier|final
+name|int
+name|DEFAULT_NUMBER_LOG_WRITER_THREAD
+init|=
+literal|10
+decl_stmt|;
 comment|// Always store the location of the root table's HRegion.
 comment|// This HRegion is never split.
 comment|// region name = table + startkey + regionid. This is the row key.
