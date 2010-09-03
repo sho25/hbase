b580d5a0278189414e32846287a9237ac6813187
Michael Stack <stack@apache.org>
2010-09-03 19:18:39 +0000

    HBASE-2955 hbase-2692 broke replication -- disabling replication tests till this issue is addressed
    git-svn-id: https://svn.apache.org/repos/asf/hbase/trunk@992425 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 6f4e9c71178ecfc8b49e6f70a59e0c2478dfae1b

--- src/test/java/org/apache/hadoop/hbase/replication/regionserver/TestReplicationSourceManager.java
+++ src/test/java/org/apache/hadoop/hbase/replication/regionserver/TestReplicationSourceManager.java
@@ -373,6 +373,16 @@ name|org
 operator|.
 name|junit
 operator|.
+name|Ignore
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
 name|Test
 import|;
 end_import
