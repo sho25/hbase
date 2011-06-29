19e10e90e20fbc19c5b75fd3b4252e418a1e68dd
Zhihong Yu <tedyu@apache.org>
2011-06-29 22:54:41 +0000

    HBASE-4037 add timeout for TestFullLogReconstruction
    git-svn-id: https://svn.apache.org/repos/asf/hbase/trunk@1141304 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: f0120d5a47dc59b32ede92454a96c214380f2d19

--- src/test/java/org/apache/hadoop/hbase/TestFullLogReconstruction.java
+++ src/test/java/org/apache/hadoop/hbase/TestFullLogReconstruction.java
@@ -337,6 +337,11 @@ block|{   }
 comment|/**    * Test the whole reconstruction loop. Build a table with regions aaa to zzz    * and load every one of them multiple times with the same date and do a flush    * at some point. Kill one of the region servers and scan the table. We should    * see all the rows.    * @throws Exception    */
 annotation|@
 name|Test
+argument_list|(
+name|timeout
+operator|=
+literal|300000
+argument_list|)
 specifier|public
 name|void
 name|testReconstruction
