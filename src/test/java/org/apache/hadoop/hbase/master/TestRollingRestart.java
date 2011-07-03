1c06fb8dee0876968c41a243b3958813e9e810d4
Zhihong Yu <tedyu@apache.org>
2011-07-03 01:02:27 +0000

    Added timeout for TestRollingRestart
    git-svn-id: https://svn.apache.org/repos/asf/hbase/trunk@1142354 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 1c151dcfac5d01d768795adfe080bffbf7544c00

--- src/test/java/org/apache/hadoop/hbase/master/TestRollingRestart.java
+++ src/test/java/org/apache/hadoop/hbase/master/TestRollingRestart.java
@@ -339,6 +339,11 @@ argument_list|)
 decl_stmt|;
 annotation|@
 name|Test
+argument_list|(
+name|timeout
+operator|=
+literal|300000
+argument_list|)
 specifier|public
 name|void
 name|testBasicRollingRestart
