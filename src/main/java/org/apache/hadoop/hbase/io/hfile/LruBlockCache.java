7b1f1544965fe3eb9ca12b1e6fe2b13b27bf2b0c
Andrew Kyle Purtell <apurtell@apache.org>
2011-11-05 01:48:45 +0000

    HBASE-4745  LRU statistics thread should be a daemon
    git-svn-id: https://svn.apache.org/repos/asf/hbase/trunk@1197864 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: c775d60de6056505471d49e5027668ed02717ccf

--- src/main/java/org/apache/hadoop/hbase/io/hfile/LruBlockCache.java
+++ src/main/java/org/apache/hadoop/hbase/io/hfile/LruBlockCache.java
@@ -531,6 +531,11 @@ argument_list|(
 literal|"LRU Statistics #%d"
 argument_list|)
 operator|.
+name|setDaemon
+argument_list|(
+literal|true
+argument_list|)
+operator|.
 name|build
 argument_list|()
 argument_list|)
