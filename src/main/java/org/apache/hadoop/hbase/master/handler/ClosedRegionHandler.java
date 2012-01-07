f2bbe164c46447ace494e5cfe06615707817f16f
Michael Stack <stack@apache.org>
2012-01-07 22:16:11 +0000

    HBASE-5141 Memory leak in MonitoredRPCHandlerImpl
    git-svn-id: https://svn.apache.org/repos/asf/hbase/trunk@1228740 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 492feb2a534828c9b7a1624d8e947c3e8f7f1bcc

--- src/main/java/org/apache/hadoop/hbase/master/handler/ClosedRegionHandler.java
+++ src/main/java/org/apache/hadoop/hbase/master/handler/ClosedRegionHandler.java
@@ -411,6 +411,7 @@ argument_list|(
 name|regionInfo
 argument_list|)
 expr_stmt|;
+comment|// This below has to do w/ online enable/disable of a table
 name|assignmentManager
 operator|.
 name|removeClosedRegion
--- src/main/java/org/apache/hadoop/hbase/master/handler/ServerShutdownHandler.java
+++ src/main/java/org/apache/hadoop/hbase/master/handler/ServerShutdownHandler.java
@@ -374,12 +374,6 @@ specifier|final
 name|DeadServer
 name|deadServers
 decl_stmt|;
-specifier|private
-specifier|final
-name|boolean
-name|shouldSplitHlog
-decl_stmt|;
-comment|// whether to split HLog or not
 specifier|public
 name|ServerShutdownHandler
 parameter_list|(
@@ -506,12 +500,6 @@ literal|" is NOT in deadservers; it should be!"
 argument_list|)
 expr_stmt|;
 block|}
-name|this
-operator|.
-name|shouldSplitHlog
-operator|=
-name|shouldSplitHlog
-expr_stmt|;
 block|}
 annotation|@
 name|Override
@@ -1260,9 +1248,7 @@ operator|.
 name|getRegionNameAsString
 argument_list|()
 operator|+
-literal|" from list of regions to assign because in RIT"
-operator|+
-literal|" region state: "
+literal|" from list of regions to assign because in RIT; region state: "
 operator|+
 name|rit
 operator|.
