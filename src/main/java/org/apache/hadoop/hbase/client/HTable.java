fc960cb34c6152f1e68e7f66353d2a2e024c4818
Michael Stack <stack@apache.org>
2012-02-23 04:43:29 +0000

    HBASE-5424 HTable meet NPE when call getRegionInfo()
    git-svn-id: https://svn.apache.org/repos/asf/hbase/trunk@1292645 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 6a80cf5bf9cb75b4124b2918fd0bfaf77486ec15

--- src/main/java/org/apache/hadoop/hbase/client/HTable.java
+++ src/main/java/org/apache/hadoop/hbase/client/HTable.java
@@ -1658,7 +1658,7 @@ name|info
 init|=
 name|Writables
 operator|.
-name|getHRegionInfo
+name|getHRegionInfoOrNull
 argument_list|(
 name|rowResult
 operator|.
@@ -1674,6 +1674,13 @@ name|REGIONINFO_QUALIFIER
 argument_list|)
 argument_list|)
 decl_stmt|;
+if|if
+condition|(
+name|info
+operator|!=
+literal|null
+condition|)
+block|{
 if|if
 condition|(
 operator|!
@@ -1787,6 +1794,7 @@ argument_list|,
 name|server
 argument_list|)
 expr_stmt|;
+block|}
 block|}
 return|return
 literal|true
