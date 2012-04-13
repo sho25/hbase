674da9be7fc7201da5b51baf92f3c0f9e2ab55e5
larsh <larsh@unknown>
2012-04-13 04:13:14 +0000

    HBASE-5604 addendum, mark TestWALPlayer as LargeTest
    git-svn-id: https://svn.apache.org/repos/asf/hbase/trunk@1325609 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: ea687c181dc5857fc298eba780595438e5519d53

--- src/test/java/org/apache/hadoop/hbase/mapreduce/TestWALPlayer.java
+++ src/test/java/org/apache/hadoop/hbase/mapreduce/TestWALPlayer.java
@@ -119,6 +119,20 @@ name|hadoop
 operator|.
 name|hbase
 operator|.
+name|LargeTests
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|hadoop
+operator|.
+name|hbase
+operator|.
 name|MiniHBaseCluster
 import|;
 end_import
@@ -267,11 +281,32 @@ name|Test
 import|;
 end_import
 
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
+name|experimental
+operator|.
+name|categories
+operator|.
+name|Category
+import|;
+end_import
+
 begin_comment
 comment|/**  * Basic test for the WALPlayer M/R tool  */
 end_comment
 
 begin_class
+annotation|@
+name|Category
+argument_list|(
+name|LargeTests
+operator|.
+name|class
+argument_list|)
 specifier|public
 class|class
 name|TestWALPlayer
