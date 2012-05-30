6f640652627d9f8ebbd9736063c9780e22857b43
Michael Stack <stack@apache.org>
2012-05-30 23:51:44 +0000

    HBASE-6087 Add hbase-common module
    git-svn-id: https://svn.apache.org/repos/asf/hbase/trunk@1344510 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 17a12aa8e5ea8391b2fd5e6554bc191b4aac3d49

--- hbase-common/src/main/java/org/apache/hadoop/hbase/HConstants.java
+++ hbase-common/src/main/java/org/apache/hadoop/hbase/HConstants.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/**  * Copyright 2010 The Apache Software Foundation  *  * Licensed to the Apache Software Foundation (ASF) under one  * or more contributor license agreements.  See the NOTICE file  * distributed with this work for additional information  * regarding copyright ownership.  The ASF licenses this file  * to you under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance  * with the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one  * or more contributor license agreements.  See the NOTICE file  * distributed with this work for additional information  * regarding copyright ownership.  The ASF licenses this file  * to you under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance  * with the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 
 begin_package
@@ -1389,24 +1389,6 @@ name|String
 name|HBCK_CODE_NAME
 init|=
 literal|"HBCKServerName"
-decl_stmt|;
-specifier|public
-specifier|static
-specifier|final
-name|ServerName
-name|HBCK_CODE_SERVERNAME
-init|=
-operator|new
-name|ServerName
-argument_list|(
-name|HBCK_CODE_NAME
-argument_list|,
-operator|-
-literal|1
-argument_list|,
-operator|-
-literal|1L
-argument_list|)
 decl_stmt|;
 specifier|public
 specifier|static
