f66ca16b6ea46b00c1544da3b860fbbf1ac29030
jxiang <jxiang@unknown>
2013-04-14 15:23:44 +0000

    HBASE-8314 HLogSplitter can retry to open a 0-length hlog file
    git-svn-id: https://svn.apache.org/repos/asf/hbase/trunk@1467790 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 10a3358bed1d02db802e061d976ad66624964911

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/wal/HLogSplitter.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/wal/HLogSplitter.java
@@ -109,16 +109,6 @@ name|java
 operator|.
 name|util
 operator|.
-name|HashSet
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|util
-operator|.
 name|LinkedList
 import|;
 end_import
