2984450f3ddd822c681336d4d69a5dbcf503ba08
eclark <eclark@unknown>
2013-07-08 18:25:30 +0000

    HBASE-8879 Client Scanner spams the logs if there are lots of scanners.
    git-svn-id: https://svn.apache.org/repos/asf/hbase/trunk@1500870 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 0b0cccb61d0eca166be235f8832ba2c7df873f1a

--- hbase-client/src/main/java/org/apache/hadoop/hbase/client/ClientScanner.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/client/ClientScanner.java
@@ -493,13 +493,13 @@ if|if
 condition|(
 name|LOG
 operator|.
-name|isDebugEnabled
+name|isTraceEnabled
 argument_list|()
 condition|)
 block|{
 name|LOG
 operator|.
-name|debug
+name|trace
 argument_list|(
 literal|"Scan table="
 operator|+
