e32064134b401a09edd834f18d3857f00e8a3769
Zhihong Yu <tedyu@apache.org>
2013-10-04 20:40:40 +0000

    HBASE-9687 ClassNotFoundException is thrown when ExportSnapshot runs against hadoop cluster where HBase is not installed on the same node as resourcemanager
    git-svn-id: https://svn.apache.org/repos/asf/hbase/trunk@1529288 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: b7b14cc80a503a23fa4bce78b917ae8e2b4f97f4

--- hbase-server/src/test/java/org/apache/hadoop/hbase/snapshot/TestExportSnapshot.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/snapshot/TestExportSnapshot.java
@@ -671,6 +671,11 @@ argument_list|(
 literal|3
 argument_list|)
 expr_stmt|;
+name|TEST_UTIL
+operator|.
+name|startMiniMapReduceCluster
+argument_list|()
+expr_stmt|;
 block|}
 annotation|@
 name|AfterClass
