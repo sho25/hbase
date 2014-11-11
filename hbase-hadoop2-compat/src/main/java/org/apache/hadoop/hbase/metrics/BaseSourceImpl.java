7ecc312c9755db567094338cef59ff127858a4f6
stack <stack@apache.org>
2014-11-11 12:59:30 -0800

    HBASE-12455 Add 'description' to bean and attribute output when you do /jmx?description=true
    Former-commit-id: 804444f892d31f1a343620be0ba7886a4854c706

--- hbase-hadoop2-compat/src/main/java/org/apache/hadoop/hbase/metrics/BaseSourceImpl.java
+++ hbase-hadoop2-compat/src/main/java/org/apache/hadoop/hbase/metrics/BaseSourceImpl.java
@@ -219,10 +219,6 @@ name|inited
 init|=
 literal|false
 decl_stmt|;
-specifier|private
-name|JvmMetrics
-name|jvmMetricsSource
-decl_stmt|;
 specifier|synchronized
 name|void
 name|init
@@ -247,8 +243,6 @@ argument_list|(
 name|HBASE_METRICS_SYSTEM_NAME
 argument_list|)
 expr_stmt|;
-name|jvmMetricsSource
-operator|=
 name|JvmMetrics
 operator|.
 name|initSingleton
