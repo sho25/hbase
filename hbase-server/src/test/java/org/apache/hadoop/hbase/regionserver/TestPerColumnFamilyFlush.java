6b351b7b820315f82119268ce63c20f23e53484e
tedyu <yuzhihong@gmail.com>
2014-12-16 16:02:44 -0800

    HBASE-10201 Addendum changes TestPerColumnFamilyFlush to LargeTest
    Former-commit-id: 885b065683499540f467cb54086a3f60e64b9c8a

--- hbase-server/src/test/java/org/apache/hadoop/hbase/regionserver/TestPerColumnFamilyFlush.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/regionserver/TestPerColumnFamilyFlush.java
@@ -561,7 +561,7 @@ name|hbase
 operator|.
 name|testclassification
 operator|.
-name|MediumTests
+name|LargeTests
 import|;
 end_import
 
@@ -659,7 +659,7 @@ begin_class
 annotation|@
 name|Category
 argument_list|(
-name|MediumTests
+name|LargeTests
 operator|.
 name|class
 argument_list|)
