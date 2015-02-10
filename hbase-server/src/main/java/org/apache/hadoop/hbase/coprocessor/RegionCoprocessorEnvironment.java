e0b97cf432e4a988d5297e66f3a6ae47529df8b6
Andrew Purtell <apurtell@apache.org>
2015-02-09 17:57:40 -0800

    HBASE-12973 RegionCoprocessorEnvironment should provide HRegionInfo directly
    Former-commit-id: 1e6afa2f3c7b8515146e7bf2bce032f8f65da7ff

--- hbase-server/src/main/java/org/apache/hadoop/hbase/coprocessor/RegionCoprocessorEnvironment.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/coprocessor/RegionCoprocessorEnvironment.java
@@ -99,6 +99,20 @@ name|hadoop
 operator|.
 name|hbase
 operator|.
+name|HRegionInfo
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|hadoop
+operator|.
+name|hbase
+operator|.
 name|regionserver
 operator|.
 name|HRegion
@@ -144,6 +158,11 @@ block|{
 comment|/** @return the region associated with this coprocessor */
 name|HRegion
 name|getRegion
+parameter_list|()
+function_decl|;
+comment|/** @return region information for the region this coprocessor is running on */
+name|HRegionInfo
+name|getRegionInfo
 parameter_list|()
 function_decl|;
 comment|/** @return reference to the region server services */
