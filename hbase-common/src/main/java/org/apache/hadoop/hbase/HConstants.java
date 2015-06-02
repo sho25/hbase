8d3ea3776162cdf974f34882c74bc332a19e5a1f
Andrey Stepachev <octo47@gmail.com>
2015-06-02 10:58:59 +0100

    HBASE-13647 Default value for hbase.client.operation.timeout is too high
    Former-commit-id: 56166b669e366ada53359f82b5bf6654f5387b63

--- hbase-common/src/main/java/org/apache/hadoop/hbase/HConstants.java
+++ hbase-common/src/main/java/org/apache/hadoop/hbase/HConstants.java
@@ -851,9 +851,7 @@ specifier|final
 name|int
 name|DEFAULT_HBASE_CLIENT_OPERATION_TIMEOUT
 init|=
-name|Integer
-operator|.
-name|MAX_VALUE
+literal|1200000
 decl_stmt|;
 comment|/** Used to construct the name of the log directory for a region server */
 specifier|public
