580b630503ffbccb1748088946a81fa077e092e7
stack <stack@apache.org>
2015-09-15 22:36:05 -0700

    HBASE-14433 Set down the client executor core thread count from 256 to number of processors
    Former-commit-id: d2e338181800ae3cef55ddca491901b65259dc7f

--- hbase-client/src/main/java/org/apache/hadoop/hbase/client/ConnectionImplementation.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/client/ConnectionImplementation.java
@@ -2231,7 +2231,13 @@ name|getInt
 argument_list|(
 literal|"hbase.hconnection.threads.core"
 argument_list|,
-literal|256
+name|Runtime
+operator|.
+name|getRuntime
+argument_list|()
+operator|.
+name|availableProcessors
+argument_list|()
 argument_list|)
 argument_list|,
 literal|"-shared-"
@@ -2311,8 +2317,6 @@ argument_list|()
 operator|.
 name|availableProcessors
 argument_list|()
-operator|*
-literal|8
 expr_stmt|;
 block|}
 name|long
