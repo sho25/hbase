efc46b1b7e350b06732e3be54a71cc4c27f93792
Josh Elser <elserj@apache.org>
2015-12-07 12:51:44 -0500

    HBASE-14838 Add clarification docs to SimpleRegionNormalizer.
    Signed-off-by: Sean Busbey <busbey@cloudera.com>
    
    Former-commit-id: ca7a82fb5704af27459557168afc96e1193ac7cb

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/normalizer/SimpleRegionNormalizer.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/normalizer/SimpleRegionNormalizer.java
@@ -176,7 +176,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * Simple implementation of region normalizer.  *  * Logic in use:  *  *<ol>  *<li> get all regions of a given table  *<li> get avg size S of each region (by total size of store files reported in RegionLoad)  *<li> If biggest region is bigger than S * 2, it is kindly requested to split,  *    and normalization stops  *<li> Otherwise, two smallest region R1 and its smallest neighbor R2 are kindly requested  *    to merge, if R1 + R1&lt;  S, and normalization stops  *<li> Otherwise, no action is performed  *</ol>  */
+comment|/**  * Simple implementation of region normalizer.  *  * Logic in use:  *  *<ol>  *<li> get all regions of a given table  *<li> get avg size S of each region (by total size of store files reported in RegionLoad)  *<li> If biggest region is bigger than S * 2, it is kindly requested to split,  *    and normalization stops  *<li> Otherwise, two smallest region R1 and its smallest neighbor R2 are kindly requested  *    to merge, if R1 + R1&lt;  S, and normalization stops  *<li> Otherwise, no action is performed  *</ol>  *<p>  * Region sizes are coarse and approximate on the order of megabytes. Additionally,  * "empty" regions (less than 1MB, with the previous note) are not merged away. This  * is by design to prevent normalization from undoing the pre-splitting of a table.  */
 end_comment
 
 begin_class
