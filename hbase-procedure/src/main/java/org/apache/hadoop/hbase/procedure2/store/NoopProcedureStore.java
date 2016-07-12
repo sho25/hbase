009d9418e83b8b376e42440dfe77b50997264a7a
Matteo Bertozzi <matteo.bertozzi@cloudera.com>
2016-07-12 10:23:02 -0700

    HBASE-16092 Procedure v2 - complete child procedure support
    Former-commit-id: 8cfaa0e72101176f00fa333e6cf6cbea7b2aca44

--- hbase-procedure/src/main/java/org/apache/hadoop/hbase/procedure2/store/NoopProcedureStore.java
+++ hbase-procedure/src/main/java/org/apache/hadoop/hbase/procedure2/store/NoopProcedureStore.java
@@ -189,6 +189,22 @@ parameter_list|)
 block|{
 comment|// no-op
 block|}
+annotation|@
+name|Override
+specifier|public
+name|void
+name|delete
+parameter_list|(
+name|Procedure
+name|proc
+parameter_list|,
+name|long
+index|[]
+name|subprocs
+parameter_list|)
+block|{
+comment|// no-op
+block|}
 block|}
 end_class
 
