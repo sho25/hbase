c0825c8bcb51c146440e519f26b7e4050c362778
tedyu <yuzhihong@gmail.com>
2016-10-03 06:45:44 -0700

    HBASE-16739 Timed out exception message should include encoded region name - Revert due to misspelled word in subject
    Former-commit-id: 1c083d9794cd43b913872c4e57936ccf2e53f497

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegion.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegion.java
@@ -25650,14 +25650,6 @@ argument_list|(
 literal|"Timed out waiting for lock for row: "
 operator|+
 name|rowKey
-operator|+
-literal|" in region "
-operator|+
-name|getRegionInfo
-argument_list|()
-operator|.
-name|getEncodedName
-argument_list|()
 argument_list|)
 throw|;
 block|}
