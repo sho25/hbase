d176ae73aa93d3cad3694a551ee8c6520927606c
Jan Hentschel <jan.hentschel@ultratendency.com>
2016-11-18 20:51:40 +0100

    HBASE-17129 Removed public from methods in DataType interface
    Signed-off-by: Sean Busbey <busbey@apache.org>
    
    Former-commit-id: 3e9d1a19e25e3e9a2eba87869682cf1cb9023626

--- hbase-common/src/main/java/org/apache/hadoop/hbase/types/DataType.java
+++ hbase-common/src/main/java/org/apache/hadoop/hbase/types/DataType.java
@@ -102,31 +102,26 @@ name|T
 parameter_list|>
 block|{
 comment|/**    * Indicates whether this instance writes encoded {@code byte[]}'s    * which preserve the natural sort order of the unencoded value.    * @return {@code true} when natural order is preserved,    *         {@code false} otherwise.    */
-specifier|public
 name|boolean
 name|isOrderPreserving
 parameter_list|()
 function_decl|;
 comment|/**    * Retrieve the sort {@link Order} imposed by this data type, or null when    * natural ordering is not preserved. Value is either ascending or    * descending. Default is assumed to be {@link Order#ASCENDING}.    */
-specifier|public
 name|Order
 name|getOrder
 parameter_list|()
 function_decl|;
 comment|/**    * Indicates whether this instance supports encoding null values. This    * depends on the implementation details of the encoding format. All    * {@code DataType}s that support null should treat null as comparing    * less than any non-null value for default sort ordering purposes.    * @return {@code true} when null is supported, {@code false} otherwise.    */
-specifier|public
 name|boolean
 name|isNullable
 parameter_list|()
 function_decl|;
 comment|/**    * Indicates whether this instance is able to skip over it's encoded value.    * {@code DataType}s that are not skippable can only be used as the    * right-most field of a {@link Struct}.    */
-specifier|public
 name|boolean
 name|isSkippable
 parameter_list|()
 function_decl|;
 comment|/**    * Inform consumers how long the encoded {@code byte[]} will be.    * @param val The value to check.    * @return the number of bytes required to encode {@code val}.a    */
-specifier|public
 name|int
 name|encodedLength
 parameter_list|(
@@ -135,7 +130,6 @@ name|val
 parameter_list|)
 function_decl|;
 comment|/**    * Inform consumers over what type this {@code DataType} operates. Useful    * when working with bare {@code DataType} instances.    */
-specifier|public
 name|Class
 argument_list|<
 name|T
@@ -144,7 +138,6 @@ name|encodedClass
 parameter_list|()
 function_decl|;
 comment|/**    * Skip {@code src}'s position forward over one encoded value.    * @param src the buffer containing the encoded value.    * @return number of bytes skipped.    */
-specifier|public
 name|int
 name|skip
 parameter_list|(
@@ -153,7 +146,6 @@ name|src
 parameter_list|)
 function_decl|;
 comment|/**    * Read an instance of {@code T} from the buffer {@code src}.    * @param src the buffer containing the encoded value.    */
-specifier|public
 name|T
 name|decode
 parameter_list|(
@@ -162,7 +154,6 @@ name|src
 parameter_list|)
 function_decl|;
 comment|/**    * Write instance {@code val} into buffer {@code dst}.    * @param dst the buffer containing the encoded value.    * @param val the value to encode onto {@code dst}.    * @return number of bytes written.    */
-specifier|public
 name|int
 name|encode
 parameter_list|(
