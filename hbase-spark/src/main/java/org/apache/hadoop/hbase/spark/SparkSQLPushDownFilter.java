fda40f5720c5bf9a8ed18d2880303094c951e39d
Jerry He <jerryjch@apache.org>
2017-03-04 14:08:38 -0800

    Revert "Define public API for spark integration module" for missing JIRA number.
    This reverts commit 3dff11a381a06ccc97c0bf08686ac40cd2b12c89 [formerly 58b6d9759e97f76221ca57a0e399c056fca8ba01].
    
    
    Former-commit-id: a95570cfa08927d843f488fa798c08b1c4e47057

--- hbase-spark/src/main/java/org/apache/hadoop/hbase/spark/SparkSQLPushDownFilter.java
+++ hbase-spark/src/main/java/org/apache/hadoop/hbase/spark/SparkSQLPushDownFilter.java
@@ -69,22 +69,6 @@ name|hadoop
 operator|.
 name|hbase
 operator|.
-name|classification
-operator|.
-name|InterfaceAudience
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|apache
-operator|.
-name|hadoop
-operator|.
-name|hbase
-operator|.
 name|exceptions
 operator|.
 name|DeserializationException
@@ -294,10 +278,6 @@ comment|/**  * This filter will push down all qualifier logic given to us  * by 
 end_comment
 
 begin_class
-annotation|@
-name|InterfaceAudience
-operator|.
-name|Private
 specifier|public
 class|class
 name|SparkSQLPushDownFilter
