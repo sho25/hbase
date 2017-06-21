1081a3b171a68ee3311a93d560c771f0796f3411
Josh Elser <elserj@apache.org>
2017-06-21 12:56:19 -0400

    HBASE-18244 Temporarily disable TestShellRSGroups
    Signed-off-by: Michael Stack <stack@apache.org>
    
    Former-commit-id: a19e5673d61bbeeb8d0f42aa414567a2c930b508

--- hbase-shell/src/test/rsgroup/org/apache/hadoop/hbase/client/rsgroup/TestShellRSGroups.java
+++ hbase-shell/src/test/rsgroup/org/apache/hadoop/hbase/client/rsgroup/TestShellRSGroups.java
@@ -241,6 +241,16 @@ name|org
 operator|.
 name|junit
 operator|.
+name|Ignore
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
 name|Test
 import|;
 end_import
@@ -311,6 +321,8 @@ operator|.
 name|class
 block|}
 argument_list|)
+annotation|@
+name|Ignore
 specifier|public
 class|class
 name|TestShellRSGroups
