8e70f2eb96057ffcfc5983c39c5e910eb56a61ce
Apekshit Sharma <appy@apache.org>
2017-10-26 16:24:23 -0700

    HBASE-18905 (addendum) Ignore broken test TestFlushLifeCycleTracker.
    Change-Id: I2f139bb76bc5ec1af53d0bd485e5d038e16b3235
    
    Former-commit-id: 660ead0118ecce83667a21420285caade2acec2c

--- hbase-server/src/test/java/org/apache/hadoop/hbase/regionserver/TestFlushLifeCycleTracker.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/regionserver/TestFlushLifeCycleTracker.java
@@ -369,6 +369,16 @@ name|org
 operator|.
 name|junit
 operator|.
+name|Ignore
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
 name|Test
 import|;
 end_import
@@ -405,6 +415,8 @@ operator|.
 name|class
 block|}
 argument_list|)
+annotation|@
+name|Ignore
 specifier|public
 class|class
 name|TestFlushLifeCycleTracker
