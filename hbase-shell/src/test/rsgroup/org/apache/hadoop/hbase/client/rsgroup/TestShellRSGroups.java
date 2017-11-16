2f0cb8321794ebe9982cfa017a762d8f4c5a29e0
Guangxu Cheng <guangxucheng@gmail.com>
2017-11-16 16:33:58 +0800

    HBASE-19269 Reenable TestShellRSGroups
    Signed-off-by: Michael Stack <stack@apache.org>
    
    Former-commit-id: ca74ec774040ae78ebe4cca37730144f498ebb14

--- hbase-shell/src/test/rsgroup/org/apache/hadoop/hbase/client/rsgroup/TestShellRSGroups.java
+++ hbase-shell/src/test/rsgroup/org/apache/hadoop/hbase/client/rsgroup/TestShellRSGroups.java
@@ -241,16 +241,6 @@ name|org
 operator|.
 name|junit
 operator|.
-name|Ignore
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|junit
-operator|.
 name|Test
 import|;
 end_import
@@ -321,8 +311,6 @@ operator|.
 name|class
 block|}
 argument_list|)
-annotation|@
-name|Ignore
 specifier|public
 class|class
 name|TestShellRSGroups
