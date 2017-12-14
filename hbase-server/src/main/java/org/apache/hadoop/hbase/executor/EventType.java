af00e74142e12e6f777646d599534aafd9f2049a
zhangduo <zhangduo@apache.org>
2017-12-14 17:34:27 +0800

    HBASE-19512 Move EventType and ExecutorType from hbase-client to hbase-server
    Former-commit-id: fe316fe7acd5337459547333d9c75f45be1d01c8

--- hbase-server/src/main/java/org/apache/hadoop/hbase/executor/EventType.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/executor/EventType.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/**  *  * Licensed to the Apache Software Foundation (ASF) under one  * or more contributor license agreements.  See the NOTICE file  * distributed with this work for additional information  * regarding copyright ownership.  The ASF licenses this file  * to you under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance  * with the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one  * or more contributor license agreements.  See the NOTICE file  * distributed with this work for additional information  * regarding copyright ownership.  The ASF licenses this file  * to you under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance  * with the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 
 begin_package
@@ -465,7 +465,7 @@ name|int
 name|code
 parameter_list|)
 block|{
-comment|// Is this going to be slow?  Its used rare but still...
+comment|// Is this going to be slow? Its used rare but still...
 for|for
 control|(
 name|EventType
@@ -486,9 +486,11 @@ argument_list|()
 operator|==
 name|code
 condition|)
+block|{
 return|return
 name|et
 return|;
+block|}
 block|}
 throw|throw
 operator|new
@@ -506,7 +508,6 @@ name|isOnlineSchemaChangeSupported
 parameter_list|()
 block|{
 return|return
-operator|(
 name|this
 operator|.
 name|equals
@@ -542,7 +543,6 @@ name|EventType
 operator|.
 name|C_M_MODIFY_TABLE
 argument_list|)
-operator|)
 return|;
 block|}
 name|ExecutorType
