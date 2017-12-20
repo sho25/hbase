15b1d62ff4de25ae1819c8b914351005582ec3f2
Jan Hentschel <jan.hentschel@ultratendency.com>
2017-12-20 18:04:56 +0100

    HBASE-19566 Fixed Checkstyle errors in hbase-archetypes and enabled Checkstyle to fail on violations
    Former-commit-id: d95cf54ba644b18e35a4d88334c674df83e01c7f

--- hbase-archetypes/hbase-shaded-client-project/src/test/java/org/apache/hbase/archetypes/exemplars/shaded_client/TestHelloHBase.java
+++ hbase-archetypes/hbase-shaded-client-project/src/test/java/org/apache/hbase/archetypes/exemplars/shaded_client/TestHelloHBase.java
@@ -20,6 +20,18 @@ package|;
 end_package
 
 begin_import
+import|import static
+name|org
+operator|.
+name|junit
+operator|.
+name|Assert
+operator|.
+name|assertEquals
+import|;
+end_import
+
+begin_import
 import|import
 name|java
 operator|.
@@ -176,18 +188,6 @@ operator|.
 name|junit
 operator|.
 name|AfterClass
-import|;
-end_import
-
-begin_import
-import|import static
-name|org
-operator|.
-name|junit
-operator|.
-name|Assert
-operator|.
-name|assertEquals
 import|;
 end_import
 
