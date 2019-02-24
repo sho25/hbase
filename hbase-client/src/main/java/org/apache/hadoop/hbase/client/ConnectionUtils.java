cde3ebed8b13073e57a3eed5e5e6c1c4eadc1b15
zhangduo <zhangduo@apache.org>
2019-02-24 18:18:33 +0800

    HBASE-21944 Validate put for batch operation
    Signed-off-by: Guanghao Zhang <zghao@apache.org>
    
    Former-commit-id: aa7d3ce39fa7f477f025343b6d9cca139c462292

--- hbase-client/src/main/java/org/apache/hadoop/hbase/client/ConnectionUtils.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/client/ConnectionUtils.java
@@ -3282,7 +3282,7 @@ block|}
 block|}
 block|}
 block|}
-comment|/**    * Select the priority for the rpc call.    *<p/>    * The rules are:    *<ol>    *<li>If user set a priority explicitly, then just use it.</li>    *<li>For meta table, use {@link HConstants#META_QOS}.</li>    *<li>For other system table, use {@link HConstants#SYSTEMTABLE_QOS}.</li>    *<li>For other tables, use {@link HConstants#NORMAL_QOS}.</li>    *</ol>    * @param priority the priority set by user, can be {@link HConstants#PRIORITY_UNSET}.    * @param tableName the table we operate on    */
+comment|/**    * Select the priority for the rpc call.    *<p/>    * The rules are:    *<ol>    *<li>If user set a priority explicitly, then just use it.</li>    *<li>For system table, use {@link HConstants#SYSTEMTABLE_QOS}.</li>    *<li>For other tables, use {@link HConstants#NORMAL_QOS}.</li>    *</ol>    * @param priority the priority set by user, can be {@link HConstants#PRIORITY_UNSET}.    * @param tableName the table we operate on    */
 specifier|static
 name|int
 name|calcPriority
