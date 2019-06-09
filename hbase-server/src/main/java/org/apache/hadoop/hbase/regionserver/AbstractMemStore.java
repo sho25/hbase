1b092cda8f4d7344b568368b1f8bf872905dfa77
WenFeiYi <wenfeiyi666@gmail.com>
2019-06-10 05:28:27 +0800

    HBASE-22284 optimization StringBuilder.append of AbstractMemStore.toString #182
    Former-commit-id: 876d2acf9ac7a9859a4b2677bd93dbe0928aa377

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/AbstractMemStore.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/AbstractMemStore.java
@@ -1092,16 +1092,28 @@ operator|.
 name|append
 argument_list|(
 literal|"Segment ("
-operator|+
+argument_list|)
+operator|.
+name|append
+argument_list|(
 name|i
-operator|+
+argument_list|)
+operator|.
+name|append
+argument_list|(
 literal|") "
-operator|+
+argument_list|)
+operator|.
+name|append
+argument_list|(
 name|segment
 operator|.
 name|toString
 argument_list|()
-operator|+
+argument_list|)
+operator|.
+name|append
+argument_list|(
 literal|"; "
 argument_list|)
 expr_stmt|;
