6f4fbecbf6f98cac495b500e1c42fec769379a16
Bharath Vissapragada <bharathv@apache.org>
2020-01-09 12:27:09 -0800

    HBASE-23648: Re-use underlying connection registry in RawAsyncHBaseAdmin (#994)
    * HBASE-23648: Re-use underlying connection registry in RawAsyncHBaseAdmin
    
    No need to create and close a new registry on demand. Audited other
    usages of getRegistry() and the code looks fine.
    
    * Fix checkstyle issues in RawAsyncHBaseAdmin
    
    
    Former-commit-id: 7162c02c0dae11aa274424e6d0c8621199272440

--- hbase-client/src/main/java/org/apache/hadoop/hbase/client/RawAsyncHBaseAdmin.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/client/RawAsyncHBaseAdmin.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/**  * Licensed to the Apache Software Foundation (ASF) under one  * or more contributor license agreements.  See the NOTICE file  * distributed with this work for additional information  * regarding copyright ownership.  The ASF licenses this file  * to you under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance  * with the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+comment|/*  * Licensed to the Apache Software Foundation (ASF) under one  * or more contributor license agreements.  See the NOTICE file  * distributed with this work for additional information  * regarding copyright ownership.  The ASF licenses this file  * to you under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance  * with the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 
 begin_package
@@ -352,20 +352,6 @@ operator|.
 name|stream
 operator|.
 name|Stream
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|apache
-operator|.
-name|commons
-operator|.
-name|io
-operator|.
-name|IOUtils
 import|;
 end_import
 
@@ -10969,22 +10955,10 @@ name|CompletableFuture
 argument_list|<>
 argument_list|()
 decl_stmt|;
-comment|// For meta table, we use zk to fetch all locations.
-name|ConnectionRegistry
-name|registry
-init|=
-name|ConnectionRegistryFactory
-operator|.
-name|getRegistry
+name|addListener
 argument_list|(
 name|connection
 operator|.
-name|getConfiguration
-argument_list|()
-argument_list|)
-decl_stmt|;
-name|addListener
-argument_list|(
 name|registry
 operator|.
 name|getMetaRegionLocations
@@ -11062,14 +11036,6 @@ argument_list|)
 argument_list|)
 expr_stmt|;
 block|}
-comment|// close the registry.
-name|IOUtils
-operator|.
-name|closeQuietly
-argument_list|(
-name|registry
-argument_list|)
-expr_stmt|;
 block|}
 argument_list|)
 expr_stmt|;
@@ -19830,7 +19796,7 @@ block|}
 end_function
 
 begin_comment
-comment|/**    * Get the region info for the passed region name. The region name may be a full region name or    * encoded region name. If the region does not found, then it'll throw an UnknownRegionException    * wrapped by a {@link CompletableFuture}    * @param regionNameOrEncodedRegionName    * @return region info, wrapped by a {@link CompletableFuture}    */
+comment|/**    * Get the region info for the passed region name. The region name may be a full region name or    * encoded region name. If the region does not found, then it'll throw an UnknownRegionException    * wrapped by a {@link CompletableFuture}    * @return region info, wrapped by a {@link CompletableFuture}    */
 end_comment
 
 begin_function
