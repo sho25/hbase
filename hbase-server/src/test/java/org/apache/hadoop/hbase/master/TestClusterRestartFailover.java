debe21e9c169691d0963c95b2659689544ce18c0
stack <stack@apache.org>
2020-01-24 12:29:29 -0800

    HBASE-23735 [Flakey Tests] TestClusterRestartFailover & TestClusterRestartFailoverSplitWithoutZk
    Former-commit-id: 16904142635b751ec0986145039d6e2fa27d89b0

--- hbase-server/src/test/java/org/apache/hadoop/hbase/master/TestClusterRestartFailover.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/master/TestClusterRestartFailover.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/**  * Licensed to the Apache Software Foundation (ASF) under one  * or more contributor license agreements.  See the NOTICE file  * distributed with this work for additional information  * regarding copyright ownership.  The ASF licenses this file  * to you under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance  * with the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+comment|/*  * Licensed to the Apache Software Foundation (ASF) under one  * or more contributor license agreements.  See the NOTICE file  * distributed with this work for additional information  * regarding copyright ownership.  The ASF licenses this file  * to you under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance  * with the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 
 begin_package
@@ -319,6 +319,22 @@ name|hadoop
 operator|.
 name|hbase
 operator|.
+name|regionserver
+operator|.
+name|HRegionServer
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|hadoop
+operator|.
+name|hbase
+operator|.
 name|testclassification
 operator|.
 name|LargeTests
@@ -338,6 +354,22 @@ operator|.
 name|testclassification
 operator|.
 name|MasterTests
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|hadoop
+operator|.
+name|hbase
+operator|.
+name|util
+operator|.
+name|JVMClusterUtil
 import|;
 end_import
 
@@ -460,6 +492,7 @@ name|class
 argument_list|)
 decl_stmt|;
 specifier|private
+specifier|volatile
 specifier|static
 name|CountDownLatch
 name|SCP_LATCH
@@ -525,21 +558,57 @@ expr_stmt|;
 name|setupTable
 argument_list|()
 expr_stmt|;
-name|SERVER_FOR_TEST
-operator|=
+comment|// Find server that does not have hbase:namespace on it. This tests holds up SCPs. If it
+comment|// holds up the server w/ hbase:namespace, the Master initialization will be held up
+comment|// because this table is not online and test fails.
+for|for
+control|(
+name|JVMClusterUtil
+operator|.
+name|RegionServerThread
+name|rst
+range|:
 name|UTIL
 operator|.
 name|getHBaseCluster
 argument_list|()
 operator|.
+name|getLiveRegionServerThreads
+argument_list|()
+control|)
+block|{
+name|HRegionServer
+name|rs
+init|=
+name|rst
+operator|.
 name|getRegionServer
+argument_list|()
+decl_stmt|;
+if|if
+condition|(
+name|rs
+operator|.
+name|getRegions
 argument_list|(
-literal|0
+name|TableName
+operator|.
+name|NAMESPACE_TABLE_NAME
 argument_list|)
+operator|.
+name|isEmpty
+argument_list|()
+condition|)
+block|{
+name|SERVER_FOR_TEST
+operator|=
+name|rs
 operator|.
 name|getServerName
 argument_list|()
 expr_stmt|;
+block|}
+block|}
 name|UTIL
 operator|.
 name|waitFor
@@ -698,6 +767,13 @@ name|build
 argument_list|()
 argument_list|)
 expr_stmt|;
+name|LOG
+operator|.
+name|info
+argument_list|(
+literal|"Started cluster"
+argument_list|)
+expr_stmt|;
 name|UTIL
 operator|.
 name|waitFor
@@ -716,6 +792,15 @@ argument_list|()
 operator|.
 name|isInitialized
 argument_list|()
+argument_list|)
+expr_stmt|;
+name|LOG
+operator|.
+name|info
+argument_list|(
+literal|"Started cluster master, waiting for {}"
+argument_list|,
+name|SERVER_FOR_TEST
 argument_list|)
 expr_stmt|;
 name|UTIL
@@ -849,6 +934,13 @@ name|NO_PROC_ID
 argument_list|)
 expr_stmt|;
 comment|// Wait the SCP to finish
+name|LOG
+operator|.
+name|info
+argument_list|(
+literal|"Waiting on latch"
+argument_list|)
+expr_stmt|;
 name|SCP_LATCH
 operator|.
 name|countDown
@@ -934,6 +1026,13 @@ parameter_list|()
 throws|throws
 name|Exception
 block|{
+name|LOG
+operator|.
+name|info
+argument_list|(
+literal|"Setup cluster"
+argument_list|)
+expr_stmt|;
 name|UTIL
 operator|.
 name|startMiniCluster
@@ -964,6 +1063,13 @@ name|build
 argument_list|()
 argument_list|)
 expr_stmt|;
+name|LOG
+operator|.
+name|info
+argument_list|(
+literal|"Cluster is up"
+argument_list|)
+expr_stmt|;
 name|UTIL
 operator|.
 name|waitFor
@@ -982,6 +1088,13 @@ argument_list|()
 operator|.
 name|isInitialized
 argument_list|()
+argument_list|)
+expr_stmt|;
+name|LOG
+operator|.
+name|info
+argument_list|(
+literal|"Master is up"
 argument_list|)
 expr_stmt|;
 comment|// wait for all SCPs finished
@@ -1015,6 +1128,13 @@ name|p
 operator|instanceof
 name|ServerCrashProcedure
 argument_list|)
+argument_list|)
+expr_stmt|;
+name|LOG
+operator|.
+name|info
+argument_list|(
+literal|"No SCPs"
 argument_list|)
 expr_stmt|;
 block|}
