0862a04c4125ebe8d9bdee524254e163391eceb3
stack <stack@apache.org>
2020-02-04 12:30:09 -0800

    HBASE-23789 [Flakey Tests] ERROR [Time-limited test] balancer.HeterogeneousRegionCountCostFunction(199): cannot read rules file located at ' /tmp/hbase-balancer.rules '
    Former-commit-id: 9cc7a711e616566ecedb8620a1c8b8a29c3d27b0

--- hbase-common/src/test/java/org/apache/hadoop/hbase/HBaseCommonTestingUtility.java
+++ hbase-common/src/test/java/org/apache/hadoop/hbase/HBaseCommonTestingUtility.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/**  *  * Licensed to the Apache Software Foundation (ASF) under one  * or more contributor license agreements.  See the NOTICE file  * distributed with this work for additional information  * regarding copyright ownership.  The ASF licenses this file  * to you under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance  * with the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+comment|/*  * Licensed to the Apache Software Foundation (ASF) under one  * or more contributor license agreements.  See the NOTICE file  * distributed with this work for additional information  * regarding copyright ownership.  The ASF licenses this file  * to you under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance  * with the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 
 begin_package
@@ -415,14 +415,14 @@ argument_list|()
 argument_list|)
 return|;
 block|}
-comment|/**    * @param subdirName the name of the subdirectory in the test data directory    * @return Path to a subdirectory named {code subdirName} under    *  {@link #getDataTestDir()}. Does *NOT* create it if it does not exist.    */
+comment|/**    * @param name the name of a subdirectory or file in the test data directory    * @return Path to a subdirectory or file named {code subdirName} under    *  {@link #getDataTestDir()}. Does *NOT* create the directory or file if it does not exist.    */
 specifier|public
 name|Path
 name|getDataTestDir
 parameter_list|(
 specifier|final
 name|String
-name|subdirName
+name|name
 parameter_list|)
 block|{
 return|return
@@ -432,7 +432,7 @@ argument_list|(
 name|getDataTestDir
 argument_list|()
 argument_list|,
-name|subdirName
+name|name
 argument_list|)
 return|;
 block|}
