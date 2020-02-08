00cde765585fe625d6d2ae60b4506be6cc09c361
stack <stack@apache.org>
2020-02-08 11:09:44 -0800

    HBASE-23816 [Flakey Test] TestExportSnapshotNoCluster.testSnapshotV2WithRefsExportFileSystemState(TestExportSnapshotNoCluster.java:91) Wrong FS!
    Former-commit-id: cdc98e906c2d1a62f6b43754b4c3dc6ce7a9adec

--- hbase-mapreduce/src/test/java/org/apache/hadoop/hbase/snapshot/TestExportSnapshotNoCluster.java
+++ hbase-mapreduce/src/test/java/org/apache/hadoop/hbase/snapshot/TestExportSnapshotNoCluster.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/**  * Licensed to the Apache Software Foundation (ASF) under one  * or more contributor license agreements.  See the NOTICE file  * distributed with this work for additional information  * regarding copyright ownership.  The ASF licenses this file  * to you under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance  * with the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+comment|/*  * Licensed to the Apache Software Foundation (ASF) under one  * or more contributor license agreements.  See the NOTICE file  * distributed with this work for additional information  * regarding copyright ownership.  The ASF licenses this file  * to you under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance  * with the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 
 begin_package
@@ -338,13 +338,8 @@ name|class
 argument_list|)
 decl_stmt|;
 specifier|private
-specifier|final
 name|HBaseCommonTestingUtility
 name|testUtil
-init|=
-operator|new
-name|HBaseCommonTestingUtility
-argument_list|()
 decl_stmt|;
 specifier|private
 name|FileSystem
@@ -444,6 +439,14 @@ parameter_list|()
 throws|throws
 name|Exception
 block|{
+name|this
+operator|.
+name|testUtil
+operator|=
+operator|new
+name|HBaseCommonTestingUtility
+argument_list|()
+expr_stmt|;
 comment|// Make sure testDir is on LocalFileSystem
 name|testDir
 operator|=
