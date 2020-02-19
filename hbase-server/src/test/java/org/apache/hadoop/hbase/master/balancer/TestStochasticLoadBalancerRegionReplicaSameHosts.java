2854f91a9dc856a3c1605555c55387b00405a99d
stack <stack@apache.org>
2020-02-18 22:39:45 -0800

    HBASE-23867 [Flakey Test] TestStochasticLoadBalancerRegionReplicaSameHosts#testRegionReplicationOnMidClusterSameHosts
    Former-commit-id: 156ababa4e15c9433a209631f4101d2341d7c231

--- hbase-server/src/test/java/org/apache/hadoop/hbase/master/balancer/TestStochasticLoadBalancerRegionReplicaSameHosts.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/master/balancer/TestStochasticLoadBalancerRegionReplicaSameHosts.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/**  * Licensed to the Apache Software Foundation (ASF) under one  * or more contributor license agreements.  See the NOTICE file  * distributed with this work for additional information  * regarding copyright ownership.  The ASF licenses this file  * to you under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance  * with the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+comment|/*  * Licensed to the Apache Software Foundation (ASF) under one  * or more contributor license agreements.  See the NOTICE file  * distributed with this work for additional information  * regarding copyright ownership.  The ASF licenses this file  * to you under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance  * with the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 
 begin_package
@@ -115,7 +115,7 @@ name|hbase
 operator|.
 name|testclassification
 operator|.
-name|LargeTests
+name|MasterTests
 import|;
 end_import
 
@@ -131,7 +131,7 @@ name|hbase
 operator|.
 name|testclassification
 operator|.
-name|MasterTests
+name|MediumTests
 import|;
 end_import
 
@@ -178,7 +178,7 @@ name|MasterTests
 operator|.
 name|class
 block|,
-name|LargeTests
+name|MediumTests
 operator|.
 name|class
 block|}
@@ -208,7 +208,6 @@ argument_list|)
 decl_stmt|;
 annotation|@
 name|Test
-comment|// Test is flakey. TODO: Fix!
 specifier|public
 name|void
 name|testRegionReplicationOnMidClusterSameHosts
@@ -256,14 +255,14 @@ expr_stmt|;
 name|int
 name|numHosts
 init|=
-literal|100
+literal|30
 decl_stmt|;
 name|int
 name|numRegions
 init|=
-literal|100
+literal|30
 operator|*
-literal|100
+literal|30
 decl_stmt|;
 name|int
 name|replication
