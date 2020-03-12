a4af70c6d86f00f9e614c2668782b1ab9bd59c31
stack <stack@apache.org>
2020-03-11 21:00:35 -0700

    HBASE-23956 Use less resources running tests (#1266) Addendum: Two more tests that had issues because parent cut down the number of default RegionServers launched.
    Former-commit-id: 20658c5139552a3409163bc364362e51ea41a1dc

--- hbase-server/src/test/java/org/apache/hadoop/hbase/replication/TestReplicationChangingPeerRegionservers.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/replication/TestReplicationChangingPeerRegionservers.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/**  * Licensed to the Apache Software Foundation (ASF) under one  * or more contributor license agreements.  See the NOTICE file  * distributed with this work for additional information  * regarding copyright ownership.  The ASF licenses this file  * to you under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance  * with the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+comment|/*  * Licensed to the Apache Software Foundation (ASF) under one  * or more contributor license agreements.  See the NOTICE file  * distributed with this work for additional information  * regarding copyright ownership.  The ASF licenses this file  * to you under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance  * with the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 
 begin_package
@@ -98,6 +98,20 @@ operator|.
 name|hbase
 operator|.
 name|MiniHBaseCluster
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|hadoop
+operator|.
+name|hbase
+operator|.
+name|Waiter
 import|;
 end_import
 
@@ -447,6 +461,11 @@ name|class
 argument_list|)
 decl_stmt|;
 annotation|@
+name|SuppressWarnings
+argument_list|(
+literal|"checkstyle:VisibilityModifier"
+argument_list|)
+annotation|@
 name|Parameter
 argument_list|(
 literal|0
@@ -752,6 +771,56 @@ operator|.
 name|getMiniHBaseCluster
 argument_list|()
 decl_stmt|;
+comment|// This test wants two RS's up. We only run one generally so add one.
+name|peerCluster
+operator|.
+name|startRegionServer
+argument_list|()
+expr_stmt|;
+name|Waiter
+operator|.
+name|waitFor
+argument_list|(
+name|peerCluster
+operator|.
+name|getConfiguration
+argument_list|()
+argument_list|,
+literal|30000
+argument_list|,
+operator|new
+name|Waiter
+operator|.
+name|Predicate
+argument_list|<
+name|Exception
+argument_list|>
+argument_list|()
+block|{
+annotation|@
+name|Override
+specifier|public
+name|boolean
+name|evaluate
+parameter_list|()
+throws|throws
+name|Exception
+block|{
+return|return
+name|peerCluster
+operator|.
+name|getLiveRegionServerThreads
+argument_list|()
+operator|.
+name|size
+argument_list|()
+operator|>
+literal|1
+return|;
+block|}
+block|}
+argument_list|)
+expr_stmt|;
 name|int
 name|numRS
 init|=
